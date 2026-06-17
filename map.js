dcd1fafaaa040f49b63e21fdd260d994a38c20a6
fmatic <fmatic@gmail.com>
2026-06-18 02:02:15 +0300

    Site Logo

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-console.log("map.js loaded v0.5.5");
+console.log("map.js loaded v0.5.6");
 const mapInfo = document.getElementById("mapInfo");
 const mapTitle = document.getElementById("mapTitle");
 const mapBandSwitcher = document.getElementById("mapBandSwitcher");
